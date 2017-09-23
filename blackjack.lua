ff23674f527baf5ab23bb1c1880712ff220a2cb2
Seven <guillermo.jose.garcia.gonzalez@gmail.com>
2017-09-23 05:42:42 +0200

    Basic lesson finished. New game blackjack

---
+++ blackjack.lua
@@ -0,0 +1,12 @@
+-- User: Null Pointer Seven
+-- Date: 22/09/2017
+
+function
+end
+
+-- VARIABLES
+continue=true
+
+while continue do
+  
+end
--- main.lua
+++ main.lua
@@ -1,20 +1,57 @@
---
--- Created by IntelliJ IDEA.
--- User: polit
+-- User: Null Pointer Seven
 -- Date: 12/09/2017
--- Time: 20:28
--- To change this template use File | Settings | File Templates.
---
 
--- defines a factorial function
-function fact(n)
-    if n == 0 then
-        return 1;
-    else
-        return n * fact(n-1);
-    end
+function first_separator()
+  print("══════════════════════════════════════════════════════\n")
 end
 
-print("enter a number:");
-a = io.read("*n");   -- read a number
-print(fact(5));
+function last_separator()
+  print("\n══════════════════════════════════════════════════════")
+end
+
+function middle_separator()
+  print("────────────────────────────────────────────────────────")
+end
+
+function calcula(number_base)
+  return number_base^210
+end
+
+function pedos()
+  return "prrrrrr prrrrrr"
+end
+
+function fact(n)
+  result=1
+  for i=n,1,-1 do
+    result=result*i
+  end
+  return result
+end
+
+function build_table(x, y)
+  io.write("┌─" ..string.rep("─",y*2).. "┐\n")
+  for i=1,x,1 do
+    io.write("│")
+    for j=1,y,1 do
+      io.write(" ".. math.random(9))
+    end
+    print(" │")
+  end
+  io.write("└─" ..string.rep("─",y*2).. "┘\n")
+end
+
+first_separator()
+
+--print("Resultado 1: ".. pedos())
+
+--middle_separator()
+
+--print("Resultado 2: " .. fact(3))
+
+--middle_separator()
+
+print("Resultado 3:\n")
+build_table(8,4)
+
+last_separator()
