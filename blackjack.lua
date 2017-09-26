31b3c778a0485bd6eecb59568050a22bc53feba2
Seven <guillermo.jose.garcia.gonzalez@gmail.com>
2017-09-26 19:36:40 +0200

    loop functions created

--- blackjack.lua
+++ blackjack.lua
@@ -8,6 +8,13 @@ players={
   ["names"]={},
   ["score"]={}
 }
+cards={
+  "A_clubs","2_clubs","3_clubs","4_clubs","5_clubs","6_clubs","7_clubs","8_clubs","9_clubs","10_clubs","J_clubs","Q_clubs","K_clubs",
+  "A_spades","2_spades","3_spades","4_spades","5_spades","6_spades","7_spades","8_spades","9_spades","10_spades","J_spades","Q_spades","K_spades",
+  "A_diamonds","2_diamonds","3_diamonds","4_diamonds","5_diamonds","6_diamonds","7_diamonds","8_diamonds","9_diamonds","10_diamonds","J_diamonds","Q_diamonds","K_diamonds",
+  "A_hearts","2_hearts","3_hearts","4_hearts","5_hearts","6_hearts","7_hearts","8_hearts","9_hearts","10_hearts","J_hearts","Q_hearts","K_hearts"
+}
+
 -- Max players: 7
 numberOfPlayers=3
 
@@ -33,19 +40,38 @@ function isGameFinished()
   
 end
 
+-- FUNCIÓN POR MODIFICAR
 function selectPlayerName()
-  local name=Guillermo
+  local name="Guillermo"
   --print("Introduce tu nombre: ")
   --local name=io.read()
   return name
+end
+
+function bets()
+  
+end
+
+function dealCards()
+  
+end
+
+function playersRound()
+  
+end
+
+function checkMoves()
+  
+end
+
+function distributeBetting()
+  
 end
 
 
 -- MAIN BLOCK PROGRAM --
 
 initializePlayersVariable(numberOfPlayers)
-
---printPlayersName()
 
 while isGameFinished()==false do
   -- Apuestas jugadores
